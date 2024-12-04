31b69fe386e8fa7ce0b4b783c3d713f595640d07
pinje <melvinaustinn@gmail.com>
2024-12-05 01:45:49 +0700

    change header card <a> to <Link>

--- src/Components/Fragments/CardProduct.jsx
+++ src/Components/Fragments/CardProduct.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Button from "../Elements/Button";
 
 const CardProduct = (props) => {
@@ -10,12 +11,12 @@ const CardProduct = (props) => {
 };
 
 const Header = (props) => {
-  const { image } = props;
+  const { image, id } = props;
   return (
     // Header
-    <a href="">
+    <Link to={`/product/${id}`}>
       <img src={image} alt="product" className="p-5 rounded-t-lg h-60 w-full object-cover" />
-    </a>
+    </Link>
   );
 };
 
