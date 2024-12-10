e722496cc2200553ce0a882561632e33f14e3d55
pinje <melvinaustinn@gmail.com>
2024-12-11 00:59:03 +0700

    Refactoring props code to redux and remove unused code

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,43 +1,13 @@
-import { Fragment, useEffect, useRef, useState } from "react";
+import { Fragment, useEffect, useState } from "react";
 import CardProduct from "../Components/Fragments/CardProduct";
-import Button from "../Components/Elements/Button";
 import { getProducts } from "../services/product.service";
 import { useLogin } from "../hooks/useLogin";
-// import Counter from "../Components/Fragments/Counter";
-
-// const products = [
-//   {
-//     id: 1,
-//     name: "Katana 1",
-//     price: 1000000,
-//     image: "/images/katana1.jpg",
-//     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime accusantium nihil reprehenderit aliquid cum eum voluptatum dolore dolorem quibusdam qui quis sint magnam ratione, non nesciunt expedita laudantium veritatis velit`,
-//   },
-//   {
-//     id: 2,
-//     name: "Katana 2",
-//     price: 500000,
-//     image: "/images/katana1.jpg",
-//     description: `Lorem ipsum dolor sit amet aliquid cum eum voluptatum dolore dolorem quibusdam qui quis sint magnam ratione, non nesciunt expedita laudantium veritatis velit`,
-//   },
-//   {
-//     id: 3,
-//     name: "Katana 3",
-//     price: 300000,
-//     image: "/images/katana1.jpg",
-//     description: `quibusdam qui quis sint magnam ratione, non nesciunt expedita laudantium veritatis velit`,
-//   },
-// ];
+import TableCart from "../Components/Fragments/TableCart";
+import Navbar from "../Components/Layouts/Navbar";
 
 const ProductsPage = () => {
-  const [cart, setCart] = useState([]);
-  const [totalPrice, setTotalPrice] = useState(0);
   const [products, setProducts] = useState([]);
-  const username = useLogin();
-
-  useEffect(() => {
-    setCart(JSON.parse(localStorage.getItem("cart")) || []);
-  }, []);
+  useLogin();
 
   useEffect(() => {
     getProducts((data) => {
@@ -45,56 +15,11 @@ const ProductsPage = () => {
     });
   }, []);
 
-  useEffect(() => {
-    if (products.length > 0 && cart.length > 0) {
-      const sum = cart.reduce((acc, item) => {
-        const product = products.find((product) => product.id === item.id);
-        return acc + product.price * item.qty;
-      }, 0);
-      setTotalPrice(sum);
-      localStorage.setItem("cart", JSON.stringify(cart));
-    }
-  }, [cart, products]);
 
-  const handleLogout = () => {
-    localStorage.removeItem("token");
-    window.location.href = "/login";
-  };
-
-  const handleAddToCart = (id) => {
-    if (cart.find((item) => item.id === id)) {
-      setCart(cart.map((item) => (item.id === id ? { ...item, qty: item.qty + 1 } : item)));
-    } else {
-      setCart([...cart, { id, qty: 1 }]);
-    }
-  };
-
-  // useRef
-  // const cartRef = useRef(JSON.parse(localStorage.getItem("cart"))) || [];
-
-  // const handleAddToCartRef = (id) => {
-  //   cartRef.current = [...cartRef.current, { id, qty: 1 }];
-  //   localStorage.setItem("cart", JSON.stringify(cartRef.current));
-  // };
-
-  const cardTotalRef = useRef(null);
-
-  useEffect(() => {
-    if (cart.length > 0) {
-      cardTotalRef.current.style.display = "table-row";
-    } else {
-      cardTotalRef.current.style.display = "none";
-    }
-  });
 
   return (
     <Fragment>
-      <div className="flex justify-end h-20 bg-primary text-white items-center px-10">
-        {username}
-        <Button classname="bg-red-600 ml-5" onClick={handleLogout}>
-          Logout
-        </Button>
-      </div>
+      <Navbar />
       <div className="flex justify-center py-5">
         <div className="w-4/6 flex flex-wrap ml-3">
           {products.length > 0 &&
@@ -103,62 +28,16 @@ const ProductsPage = () => {
                 <CardProduct key={product.id}>
                   <CardProduct.Header image={product.image} id={product.id} />
                   <CardProduct.Body name={product.title}>{product.description}</CardProduct.Body>
-                  <CardProduct.Footer price={product.price} id={product.id} handleAddToCart={handleAddToCart} />
+                  <CardProduct.Footer price={product.price} id={product.id} />
                 </CardProduct>
               );
             })}
         </div>
         <div className="w-2/6">
           <h1 className="text-3xl font-bold text-emerald-600 ml-5 mb-2">Cart</h1>
-          {/* <ul>
-            using "()" after => to return the jsx
-            {cart.map((item) => (
-              <li key={item}>{item.id}</li>
-            ))}
-
-            using "{}" after => need return statement
-            {cart.map((item) => {
-              return <li key={item.name}>{item.name}</li>;
-            })}
-          </ul> */}
-          <table className="text-left table-auto border-separate border-spacing-x-5 ">
-            <thead>
-              <tr>
-                <th>Product</th>
-                <th>Price</th>
-                <th>Quantity</th>
-                <th>Total</th>
-              </tr>
-            </thead>
-            <tbody>
-              {products.length > 0 &&
-                cart.map((item) => {
-                  const product = products.find((product) => product.id === item.id);
-                  return (
-                    <tr key={item.id}>
-                      <td>{product.title.substring(0, 10)}...</td>
-                      <td>${product.price.toLocaleString("en-US", { styles: "currency", currency: "USD" })}</td>
-                      <td className="text-center">{item.qty}</td>
-                      <td>
-                        {(product.price * item.qty).toLocaleString("en-US", { style: "currency", currency: "USD" })}
-                      </td>
-                    </tr>
-                  );
-                })}
-              {/* useState */}
-              <tr ref={cardTotalRef}>
-                <td colSpan={3}>
-                  <b>Total Price</b>
-                </td>
-                <td>
-                  <b>${totalPrice.toLocaleString("en-US", { styles: "currency", currency: "USD" })}</b>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <TableCart products={products} />
         </div>
       </div>
-      {/* <Counter></Counter> */}
     </Fragment>
   );
 };
