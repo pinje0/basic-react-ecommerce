3543a64ea2e005a2e7e038aef1c6cd186edd38d7
pinje <melvinaustinn@gmail.com>
2024-12-11 00:56:51 +0700

    Refactoring &  Change addToCart from props to redux state

--- src/Components/Fragments/CardProduct.jsx
+++ src/Components/Fragments/CardProduct.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom";
 import Button from "../Elements/Button";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../../redux/slices/cartSlice";
 
 const CardProduct = (props) => {
   const { children } = props;
@@ -34,14 +36,15 @@ const Body = (props) => {
 };
 
 const Footer = (props) => {
-  const { price, handleAddToCart, id } = props;
+  const { price, id } = props;
+  const dispatch = useDispatch();
   return (
     //  Footer
     <div className="flex items-center justify-between px-5 pb-5">
       <span className="text-xl font-bold text-white">
         ${price.toLocaleString("id-ID", { styles: "currency", currency: "IDR" })}
       </span>
-      <Button classname="bg-emerald-600" onClick={() => handleAddToCart(id)}>
+      <Button classname="bg-emerald-600" onClick={() => dispatch(addToCart({ id, qty: 1 }))}>
         Add to cart
       </Button>
     </div>
