651676a899b7539301e3ba9801024bbd55e5a10d
pinje <melvinaustinn@gmail.com>
2024-12-11 15:06:50 +0700

    change default text color to black

--- src/Components/Elements/Input/Label.jsx
+++ src/Components/Elements/Input/Label.jsx
@@ -1,7 +1,7 @@
 const Label = (props) => {
   const { htmlFor, children } = props;
   return (
-    <label htmlFor={htmlFor} className="block text-slate-300 text-sm font-bold mb-2">
+    <label htmlFor={htmlFor} className="block  text-sm font-bold mb-2">
       {children}
     </label>
   );
