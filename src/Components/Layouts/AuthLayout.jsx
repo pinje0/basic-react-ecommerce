1dd013f1303c0c25ff4839e8037ff17fb92c2d20
pinje <melvinaustinn@gmail.com>
2024-12-11 15:08:47 +0700

    add dark mode using useContext

--- src/Components/Layouts/AuthLayout.jsx
+++ src/Components/Layouts/AuthLayout.jsx
@@ -1,12 +1,25 @@
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { DarkMode } from "../../context/DarkMode";
 
 const AuthLayout = (props) => {
   const { children, title, type } = props;
+  const { isDarkMode, setIsDarkMode } = useContext(DarkMode);
+  console.log(isDarkMode);
+
   return (
-    <div className="flex justify-center min-h-screen items-center bg-primary">
+    <div
+      className={`flex justify-center min-h-screen items-center ${isDarkMode && "bg-primary text-slate-400"}`}
+    >
       <div className="w-full max-w-xs">
+        <button
+          className="absolute right-10 top-5 bg-emerald-600 p-2  text-white rounded hover-opacity-animation"
+          onClick={() => setIsDarkMode(!isDarkMode)}
+        >
+          {isDarkMode ? "Light" : "Dark"}
+        </button>
         <h1 className="text-3xl font-bold mb-2 text-emerald-600">{title}</h1>
-        <p className="font-medium text-slate-400 mb-8">Welcome, Please enter your details</p>
+        <p className="font-medium  mb-8">Welcome, Please enter your details</p>
         {children}
         <Navigation type={type} />
       </div>
@@ -17,7 +30,7 @@ const AuthLayout = (props) => {
 const Navigation = ({ type }) => {
   if (type === "login") {
     return (
-      <p className="text-slate-400 mt-3">
+      <p className=" mt-3">
         Don&apos;t have an account?{" "}
         <Link to="/register" className="text-emerald-600 font-bold hover-opacity-animation">
           Register
@@ -26,7 +39,7 @@ const Navigation = ({ type }) => {
     );
   } else if (type === "register") {
     return (
-      <p className="text-slate-400 mt-3">
+      <p className=" mt-3">
         Already have an account?{" "}
         <Link to="/login" className="text-emerald-600 font-bold hover-opacity-animation">
           Login
