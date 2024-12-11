9fdeb454fec2188fc7a4d58b45b1bb191ded9ecf
pinje <melvinaustinn@gmail.com>
2024-12-12 00:30:24 +0700

    add total cart to navbar

--- src/Components/Layouts/Navbar.jsx
+++ src/Components/Layouts/Navbar.jsx
@@ -3,12 +3,14 @@ import { useLogin } from "../../hooks/useLogin";
 import Button from "../Elements/Button";
 import { useSelector } from "react-redux";
 import { DarkMode } from "../../context/DarkMode";
+import { useTotalPrice } from "../../context/TotalPriceContext";
 
 const Navbar = () => {
   const username = useLogin();
   const { isDarkMode, setIsDarkMode } = useContext(DarkMode);
   const [totalCart, setTotalCart] = useState(0);
   const cart = useSelector((state) => state.cart.data);
+  const { total } = useTotalPrice();
 
   useEffect(() => {
     const sum = cart.reduce((acc, item) => {
@@ -28,7 +30,9 @@ const Navbar = () => {
       <Button classname="bg-red-600 ml-5" onClick={handleLogout}>
         Logout
       </Button>
-      <div className="flex items-center bg-gray-800 p-2 rounded-md mx-5">{totalCart}</div>
+      <div className="flex items-center bg-gray-800 p-2 rounded-md mx-5">
+        Item : {totalCart} | Price : $ {total}
+      </div>
       <Button
         className="bg-primary px-10 m-5 text-white rounded hover-opacity-animation"
         onClick={() => setIsDarkMode(!isDarkMode)}
