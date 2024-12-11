9597454a6314e9cc658591f465b16ae6671401c3
pinje <melvinaustinn@gmail.com>
2024-12-11 15:09:25 +0700

    add button toggle for dark mode

--- src/Components/Layouts/Navbar.jsx
+++ src/Components/Layouts/Navbar.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLogin } from "../../hooks/useLogin";
 import Button from "../Elements/Button";
 import { useSelector } from "react-redux";
+import { DarkMode } from "../../context/DarkMode";
 
 const Navbar = () => {
   const username = useLogin();
+  const { isDarkMode, setIsDarkMode } = useContext(DarkMode);
   const [totalCart, setTotalCart] = useState(0);
   const cart = useSelector((state) => state.cart.data);
 
@@ -21,12 +23,18 @@ const Navbar = () => {
   };
 
   return (
-    <div className="flex justify-end h-20 bg-primary text-white items-center px-10">
+    <div className="flex justify-end h-20 bg-emerald-600 text-white items-center px-10">
       {username}
       <Button classname="bg-red-600 ml-5" onClick={handleLogout}>
         Logout
       </Button>
-      <div className="flex items-center bg-gray-800 p-2 rounded-md ml-5">{totalCart}</div>
+      <div className="flex items-center bg-gray-800 p-2 rounded-md mx-5">{totalCart}</div>
+      <Button
+        className="bg-primary px-10 m-5 text-white rounded hover-opacity-animation"
+        onClick={() => setIsDarkMode(!isDarkMode)}
+      >
+        {isDarkMode ? "Light" : "Dark"}
+      </Button>
     </div>
   );
 };
